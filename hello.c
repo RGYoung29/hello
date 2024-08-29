7b95271a15714e8879a299aafe6618133c4b1a13
RGYoung29 <ryoung29@huskers.unl.edu>
2024-08-29 13:42:53 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Date: 2024/08/29
+ * Name: Ronnie G Young
+ *
+ * Hack 1.0 - Getting Started with Git
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Ronnie Young's Hack 1.0!\n");
+    printf("Major: Mechanical or Computer Engineering\n");
+
+    return 0;
+}
+
